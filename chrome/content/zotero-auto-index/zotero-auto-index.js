b1380ce79f803142e74281cfe5ef5fe14b59f6df
friflaj <friflaj@ajellito.com>
2014-03-19 21:10:47 +0100

    0.0.27

--- chrome/content/zotero-auto-index/zotero-auto-index.js
+++ chrome/content/zotero-auto-index/zotero-auto-index.js
@@ -209,7 +209,4 @@ Zotero.AutoIndex = {
 };
 
 // Initialize the utility
-window.addEventListener('load', function(e) {
-  Zotero.Fulltext.init();
-  Zotero.AutoIndex.init();
-}, false);
+window.addEventListener('load', function(e) { Zotero.AutoIndex.init(); }, false);
